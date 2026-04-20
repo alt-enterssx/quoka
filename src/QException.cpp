c528b6e3fafd2dbc0de74b5515ab7296800c0051
altenter <altenter.codi@gmail.com>
2026-04-20 22:29:32 +0600

    fix: delete pervious code

--- src/QException.cpp
+++
@@ -1,7 +0,0 @@
-#include "altenter/QException.h"
-
-using namespace altenter::quokahttp;
-
-QException::QException(const std::string&& msg, altenter::quokahttp::ExceptionType type): std::runtime_error(msg), exc_type(type) {}
-
-ExceptionType QException::getType() { return this->exc_type; }
